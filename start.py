bead5b6dd187a481445752765afe1ac6387ff114
kurgxn <kurgxn@gmail.com>
2023-12-25 20:40:36 +0300

    Проект разбит по модулям

---
+++ start.py
@@ -0,0 +1,11 @@
+from modules.main_module import MainWindow
+
+from PyQt6.QtWidgets import QApplication
+import sys
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
